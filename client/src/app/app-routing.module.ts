0261dfbdd3184ac51709d1baf38312e8ff0d6e05
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-11-10 23:44:18 +0800

    first commit

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'reg',
+    loadChildren: () => import('./reg/reg.module').then( m => m.RegPageModule)
+  },
+  {
+    path: 'option',
+    loadChildren: () => import('./option/option.module').then( m => m.OptionPageModule)
+  },
+  {
+    path: 'person',
+    loadChildren: () => import('./person/person.module').then( m => m.PersonPageModule)
+  },
+  {
+    path: 'world',
+    loadChildren: () => import('./world/world.module').then( m => m.WorldPageModule)
+  },
+  {
+    path: 'friends',
+    loadChildren: () => import('./friends/friends.module').then( m => m.FriendsPageModule)
+  },
+  {
+    path: 'forgetpassword',
+    loadChildren: () => import('./forgetpassword/forgetpassword.module').then( m => m.ForgetpasswordPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
